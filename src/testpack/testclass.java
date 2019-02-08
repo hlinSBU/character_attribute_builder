4935c392f9e5d19ba026ff27e7fbe589eff90f01
Hope <hsinying.lin@stonybrook.edu>
2019-02-08 16:24:29 -0500

    test push

---
+++ src/testpack/testclass.java
@@ -0,0 +1,33 @@
+package testpack;
+
+public class testclass {
+	public String abc;
+	public int a;
+	public double b;
+	public testclass(String abc, int a, double b) {
+		super();
+		this.abc = abc;
+		this.a = a;
+		this.b = b;
+	}
+	public String getAbc() {
+		return abc;
+	}
+	public void setAbc(String abc) {
+		this.abc = abc;
+	}
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public double getB() {
+		return b;
+	}
+	public void setB(double b) {
+		this.b = b;
+	}
+	
+	
+}
